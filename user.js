fc62b98889f9cfef95dccff53acb199844765023
stretfordStart <116835036+stretfordStart@users.noreply.github.com>
2023-10-09 11:21:20 +0200

    Create user.js

---
+++ user.js
@@ -0,0 +1,5 @@
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.startup.homepage_override.toplevel", "about:blank");
+user_pref("startup.homepage_welcome_url", "https://stretfordstart.github.io/soc_workstation_doc/");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("browser.shell.checkDefaultBrowser", false);
