17f1b6f64b4499d1a399dc9bc522f145814537d4
stretfordStart <116835036+stretfordStart@users.noreply.github.com>
2023-10-13 09:38:58 +0200

    Update virtual.sh

--- virtual.sh
+++ virtual.sh
@@ -16,24 +16,26 @@ if ! command -v qemu-img &>/dev/null; then
     sudo apt-get install qemu-utils
 fi
 
-# Create a QEMU VM
-echo "Creating QEMU VM..."
-qemu-img create -f qcow2 "$VM_NAME.qcow2" "$VM_DISK_SIZE"
+# Convert the OVA to a QCOW2 format
+echo "Converting OVA to QCOW2..."
+qemu-img convert -f ova -O qcow2 remnux.ova "$VM_NAME.qcow2"
 
-# Install the VM using the REMnux ISO
-qemu-system-x86_64 \
-    -name "$VM_NAME" \
-    -m "$VM_MEMORY" \
-    -smp "$VM_CPU_CORES" \
-    -drive file="$VM_NAME.qcow2",format=qcow2 \
-    -cdrom remnux.ova \
-    -boot d \
-    -vga std \
-    -display gtk \
-    -net user,hostfwd=tcp::2222-:22 -net nic
+# Create a QEMU VM with Virt-Manager
+echo "Creating QEMU VM..."
+virt-install \
+    --name "$VM_NAME" \
+    --memory "$VM_MEMORY" \
+    --vcpus "$VM_CPU_CORES" \
+    --disk path="$VM_NAME.qcow2" \
+    --cdrom "$REMNUX_ISO_URL" \
+    --boot hd,cdrom \
+    --graphics spice \
+    --network default \
+    --os-variant ubuntu20.04 \
+    --noautoconsole
 
 # Clean up
 echo "Cleaning up..."
 rm remnux.ova
 
-echo "REMnux VM setup complete. You can connect via SSH to localhost:2222 (username: remnux, password: reverse)."
+echo "REMnux VM setup complete. You can manage it with Virt-Manager."
