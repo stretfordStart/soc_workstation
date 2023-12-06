e98a22ca9cec346f4cd648625ae0200b3b6f844c
stretfordStart <116835036+stretfordStart@users.noreply.github.com>
2023-12-06 11:23:48 +0100

    Update flare_provision.ps1

--- vms/setup/flare_provision.ps1
+++ vms/setup/flare_provision.ps1
@@ -53,7 +53,7 @@ catch {
 
 # Install software using chocolatey
 Write-Output "Installing software using chocolatey..."
-$software = @("firefox", "7zip", "libreoffice-fresh", "thunderbird", "microsoft-windows-terminal")
+$software = @("firefox", "7zip", "libreoffice-fresh")
 foreach ($sw in $software) {
   Write-Output "Installing $sw..."
   try {
