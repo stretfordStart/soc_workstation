a5a9d2a631ad83d4479c1b8f4c8081d67f5b24a2
stretfordStart <116835036+stretfordStart@users.noreply.github.com>
2023-12-06 13:44:16 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,10 +13,10 @@ configure_gnome_settings() {
     gsettings set org.gnome.desktop.screensaver user-switch-enabled false
     gsettings set org.gnome.desktop.wm.preferences button-layout appmenu:minimize,maximize,close
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-    dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'virt-manager.desktop', 'org.gnome.Nautilus.desktop', 'com.gexperts.Tilix.desktop']"
+    dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'virtualbox.desktop', 'org.gnome.Nautilus.desktop', 'com.gexperts.Tilix.desktop']"
     gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
-    gsettings set org.gnome.shell.extensions.user-theme name Flat-Remix-Blue-Darkest-fullPanel
-    gsettings set org.gnome.desktop.interface gtk-theme Flat-Remix-GTK-Blue-Darkest-Solid
+    gsettings set org.gnome.shell.extensions.user-theme name Flat-Remix-Orange-Darkest-fullPanel
+    gsettings set org.gnome.desktop.interface gtk-theme Flat-Remix-GTK-Orange-Darkest-Solid
     gsettings set org.gnome.desktop.interface icon-theme Tela-circle
 }
 
