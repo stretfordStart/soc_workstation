1a8a8bef881efacc363c4430662d3283fe55ce44
stretfordStart <116835036+stretfordStart@users.noreply.github.com>
2023-12-12 10:28:39 +0100

    Delete user.js

--- user.js
+++
@@ -1,5 +0,0 @@
-user_pref("browser.startup.homepage_override.mstone", "ignore");
-user_pref("browser.startup.homepage_override.toplevel", "about:blank");
-user_pref("startup.homepage_welcome_url", "https://stretfordstart.github.io/soc_workstation_doc/");
-user_pref("startup.homepage_welcome_url.additional", "");
-user_pref("browser.shell.checkDefaultBrowser", false);
